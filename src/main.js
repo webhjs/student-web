58866bc0df8748c711955dd7df7817c1a658d01b
webhjs <1395516797@qq.com>
2022-01-07 13:26:45 +0800

    失败

--- src/main.js
+++ src/main.js
@@ -19,23 +19,6 @@ import store from "@/store";
 // 国际化
 import i18n from "@/libs/lang";
 
-import { Form } from 'vant';
-import { Field } from 'vant';
-import { Area } from 'vant';
-import { Button } from 'vant';
-import { ActionSheet } from 'vant';
-
-Vue.use(Area);
-Vue.use(Form);
-Vue.use(Field);
-Vue.use(Button);
-Vue.use(ActionSheet);
-import 'vant/lib/form/style';
-import 'vant/lib/area/style';
-import 'vant/lib/field/style';
-import 'vant/lib/button/style';
-import 'vant/lib/action-sheet/style';
-
 Vue.use(ElementUI, {
   size: "medium",
   i18n: (key, value) => i18n.t(key, value)
